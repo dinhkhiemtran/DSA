e71cb4d5be25b263f2a4290dcec7f8d677dff97d
Khiem Tran <tdkhiem>
2022-12-20 14:46:51 +0700

    Selection Sort

---
+++ untitled/src/algorithms/sorting_and_searching/SelectionSort.java
@@ -0,0 +1,82 @@
+package algorithms.sorting_and_searching;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class SelectionSort {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    private int inputLengthOfArray() {
+        int length = 0;
+        try {
+            System.out.print("Input a length of array: ");
+            length = scanner.nextInt();
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("Exception: " + "Input wrong type.");
+        } catch (Exception exception) {
+            System.out.println("Exception: " + exception.getMessage());
+        }
+        return length;
+    }
+
+    private int[] inputElements() {
+        int length = inputLengthOfArray();
+        int[] array = new int[length];
+        try {
+            for (int i = 0; i < array.length; i++) {
+                System.out.print("Input elements: ");
+                int element = scanner.nextInt();
+                array[i] = element;
+            }
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("Exception: " + "Input wrong type.");
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+        return array;
+    }
+
+    public int[] selectionSort(int[] array) {
+        int lengthOfArray = array.length;
+        if (array != null) {
+            for (int i = 0 ; i < lengthOfArray; i++){
+                int min = i;
+                for (int j = i + 1; j < lengthOfArray; j++){
+                    if (array[min] > array[j]){
+                        min = j;
+                    }
+                }
+                if (i != min){
+                    swap(array, i, min);
+                }
+            }
+        }
+        return array;
+    }
+
+    public void swap(int[] array, int i, int min){
+        int temp = array[i];
+        array[i] = array[min];
+        array[min] = temp;
+    }
+
+    public void print(int[] array){
+        int lengthOfArray = array.length;
+        System.out.print("Sorted: ");
+        for (int i = 0; i < lengthOfArray; i++){
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void display(){
+        print(selectionSort(inputElements()));
+    }
+
+    public static void main(String[] args) {
+        SelectionSort selectionSort = new SelectionSort();
+        selectionSort.display();
+    }
+
+}
