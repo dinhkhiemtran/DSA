c9a42a2afc025e378aea2325f2f18e493659e420
Khiem Tran <tdkhiem>
2022-12-23 11:18:04 +0700

    Heap sort

--- untitled/src/algorithms/sorting_and_searching/BucketSort.java
+++ untitled/src/algorithms/sorting_and_searching/BucketSort.java
@@ -2,46 +2,95 @@ package algorithms.sorting_and_searching;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class BucketSort {
 
-    public void bucketSort(float[] arr, int n){
-        if (n <= 0)
-            return;
-        @SuppressWarnings("unchecked")
-        ArrayList<Float>[] bucket = new ArrayList[n];
+    private Scanner scanner = new Scanner(System.in);
 
-        // Create empty buckets
-        for (int i = 0; i < n; i++)
-            bucket[i] = new ArrayList<Float>();
+    public void bucketSort() {
+        float[] arr = addElement();
+        int n = arr.length;
+        try {
+            if (n <= 0)
+                return;
+            @SuppressWarnings("unchecked")
+            ArrayList<Float>[] bucket = new ArrayList[n];
 
-        // Add elements into the buckets
-        for (int i = 0; i < n; i++) {
-            int bucketIndex = (int) arr[i] * n;
-            bucket[bucketIndex].add(arr[i]);
-        }
+            // Create empty buckets
+            for (int i = 0; i < n; i++)
+                bucket[i] = new ArrayList<Float>();
 
-        // Sort the elements of each bucket
-        for (int i = 0; i < n; i++) {
-            Collections.sort((bucket[i]));
-        }
-
-        // Get the sorted array
-        int index = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0, size = bucket[i].size(); j < size; j++) {
-                arr[index++] = bucket[i].get(j);
+            // Add elements into the buckets
+            for (int i = 0; i < n; i++) {
+                int bucketIndex = (int) arr[i] * n;
+                bucket[bucketIndex].add(arr[i]);
             }
+
+            // Sort the elements of each bucket
+            for (int i = 0; i < n; i++) {
+                Collections.sort((bucket[i]));
+            }
+
+            // Get the sorted array
+            int index = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0, size = bucket[i].size(); j < size; j++) {
+                    arr[index++] = bucket[i].get(j);
+                }
+            }
+        } catch (Exception exception) {
+            System.out.println("Exception: " + exception.getMessage());
         }
+        print(arr);
+    }
+
+    private int createSize() {
+        int size = 0;
+        try {
+            System.out.print("Create a size of array: ");
+            size = scanner.nextInt();
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("Exception: " + "Input wrong type.");
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex.getMessage());
+        }
+        return size;
+    }
+
+    private float[] addElement() {
+        int size = createSize();
+        float[] array = new float[size];
+        try {
+            for (int i = 0; i < size; i++) {
+                System.out.print("Add element: ");
+                float element = scanner.nextFloat();
+                array[i] = element;
+            }
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("Exception: " + "Input wrong type.");
+        } catch (Exception exception) {
+            System.out.println("Exception: " + exception.getMessage());
+        }
+        return array;
+    }
+
+    private void print(float[] array) {
+        System.out.print("Sorted: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
         BucketSort bucketSort = new BucketSort();
-        float[] arr = { (float) 0.42, (float) 0.32, (float) 0.33, (float) 0.52, (float) 0.37, (float) 0.47,
-                (float) 0.51 };
-        bucketSort.bucketSort(arr, 7);
+//        float[] arr = {(float) 0.42, (float) 0.32, (float) 0.33, (float) 0.52, (float) 0.37, (float) 0.47,
+//                (float) 0.51};
+        bucketSort.bucketSort();
 
-        for (float i : arr)
-            System.out.print(i + "  ");
+//        for (float i : arr)
+//            System.out.print(i + "  ");
     }
 }
---
+++ untitled/src/algorithms/sorting_and_searching/HeapSort.java
@@ -0,0 +1,102 @@
+package algorithms.sorting_and_searching;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class HeapSort {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    private void heapify(int arr[], int n, int i) {
+        //find largest among root, left child and right child
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < n && arr[l] > arr[largest]) {
+            largest = l;
+        }
+
+        if (r < n && arr[r] > arr[largest]) {
+            largest = r;
+        }
+
+        // Swap and continue heapifying if root is not largest
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    private void sort() {
+
+        int arr[] = addElements();
+
+        int n = arr.length;
+
+        // Build max heap
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // Heap sort
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // Heapify root element
+            heapify(arr, i, 0);
+        }
+        print(arr);
+    }
+
+    private int createSizeOfArray() {
+        int size = 0;
+        try {
+            System.out.print("Create a size of array: ");
+            size = scanner.nextInt();
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("");
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex.getMessage());
+        }
+        return size;
+    }
+
+    private int[] addElements() {
+        int size = createSizeOfArray();
+        int[] array = new int[size];
+        try {
+            for (int i = 0 ; i < size; i++){
+                System.out.print("Add elements: ");
+                int element = scanner.nextInt();
+                array[i] = element;
+            }
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("Exception: " + "Input wrong type.");
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex.getMessage());
+        }
+        return array;
+    }
+
+    private void print(int[] array){
+        System.out.print("Sorted: ");
+        for (int i = 0; i < array.length; i++){
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    private void display(){
+        sort();
+    }
+
+    public static void main(String[] args) {
+        HeapSort heapSort = new HeapSort();
+        heapSort.display();
+    }
+}
