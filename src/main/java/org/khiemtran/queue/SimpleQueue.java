7166b6c7169d9115328bd1a6ad099ba6d9aaedf5
dinhkhiemtran <dinhkhiemtran@gmail.com>
2024-03-03 22:08:35 +0700

    Simple Queue Structure

---
+++ src/main/java/org/khiemtran/queue/SimpleQueue.java
@@ -0,0 +1,70 @@
+package org.khiemtran.queue;
+
+public class SimpleQueue {
+  private int rear;
+  private int front;
+  private int[] items;
+  private int size;
+
+  public SimpleQueue(int capacity) {
+    this.rear = -1;
+    this.front = -1;
+    this.size = 0;
+    this.items = new int[capacity];
+  }
+
+  public int getRear() {
+    return rear;
+  }
+
+  public int getFront() {
+    return front;
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+  public void add(int element) {
+    if (isFull()) {
+      System.out.println("Queue is full.");
+    } else {
+      if (front == -1) {
+        front = 0;
+      }
+      size++;
+      items[++rear] = element;
+    }
+  }
+
+  public void delete() {
+    if (!isEmpty()) {
+      if (front >= rear) {
+        front = -1;
+        rear = -1;
+      } else {
+        front++;
+      }
+      size--;
+    }
+  }
+
+  public void display() {
+    if (isEmpty()) {
+      System.out.println("Queue is empty");
+    } else {
+      for (int i = front; i <= rear; i++) {
+        System.out.print(items[i] + ",");
+      }
+    }
+    System.out.println();
+  }
+
+  private boolean isEmpty() {
+    return size == 0;
+  }
+
+  private boolean isFull() {
+    return size > items.length - 1;
+  }
+}
---
+++ src/test/java/org/khiemtran/queue/SimpleQueueTest.java
@@ -0,0 +1,79 @@
+package org.khiemtran.queue;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SimpleQueueTest {
+  private SimpleQueue simpleQueue;
+  @Test
+  public void testQueueFull() {
+    simpleQueue = new SimpleQueue(1);
+    simpleQueue.add(1);
+    simpleQueue.add(2);
+    Assertions.assertEquals(1, simpleQueue.getSize());
+    Assertions.assertEquals(0, simpleQueue.getFront());
+    Assertions.assertEquals(0, simpleQueue.getRear());
+    simpleQueue.display();
+  }
+
+  @Test
+  public void testAdd() {
+    simpleQueue = new SimpleQueue(5);
+    simpleQueue.add(1);
+    simpleQueue.add(2);
+    simpleQueue.add(3);
+    simpleQueue.add(4);
+    simpleQueue.add(5);
+    Assertions.assertEquals(5, simpleQueue.getSize());
+    Assertions.assertEquals(0, simpleQueue.getFront());
+    Assertions.assertEquals(4, simpleQueue.getRear());
+    simpleQueue.display();
+  }
+
+  @Test
+  public void testDeleteWhenQueueEmpty() {
+    simpleQueue = new SimpleQueue(1);
+    simpleQueue.add(1);
+    simpleQueue.delete();
+    simpleQueue.delete();
+    Assertions.assertEquals(0, simpleQueue.getSize());
+    Assertions.assertEquals(-1, simpleQueue.getRear());
+    Assertions.assertEquals(-1, simpleQueue.getFront());
+    simpleQueue.display();
+  }
+
+  @Test
+  public void testDelete() {
+    simpleQueue = new SimpleQueue(5);
+    simpleQueue.add(1);
+    simpleQueue.add(2);
+    simpleQueue.add(3);
+    simpleQueue.add(4);
+    simpleQueue.add(5);
+    simpleQueue.display();
+    simpleQueue.delete();
+    Assertions.assertEquals(4, simpleQueue.getSize());
+    Assertions.assertEquals(1, simpleQueue.getFront());
+    Assertions.assertEquals(4, simpleQueue.getRear());
+    simpleQueue.display();
+    simpleQueue.delete();
+    Assertions.assertEquals(3, simpleQueue.getSize());
+    Assertions.assertEquals(2, simpleQueue.getFront());
+    Assertions.assertEquals(4, simpleQueue.getRear());
+    simpleQueue.display();
+    simpleQueue.delete();
+    Assertions.assertEquals(2, simpleQueue.getSize());
+    Assertions.assertEquals(3, simpleQueue.getFront());
+    Assertions.assertEquals(4, simpleQueue.getRear());
+    simpleQueue.display();
+    simpleQueue.delete();
+    Assertions.assertEquals(1, simpleQueue.getSize());
+    Assertions.assertEquals(4, simpleQueue.getFront());
+    Assertions.assertEquals(4, simpleQueue.getRear());
+    simpleQueue.display();
+    simpleQueue.delete();
+    Assertions.assertEquals(0, simpleQueue.getSize());
+    Assertions.assertEquals(-1, simpleQueue.getFront());
+    Assertions.assertEquals(-1, simpleQueue.getFront());
+  }
+}
