78d29a82d1250369b1949e9ff87dc5db125164c2
dinhkhiemtran <dinhkhiemtran@gmail.com>
2025-08-11 02:08:04 +0700

    Implement Dynamic Array

--- src/main/java/org/khiemtran/algorithms/prefix_sum/PrefixSumArray.java
+++ src/main/java/org/khiemtran/algorithms/prefix_sum/PrefixSumArray.java
@@ -1,2 +1,12 @@
-package org.khiemtran.algorithms.prefix_sum;public class PrefixSumArray {
+package org.khiemtran.algorithms.prefix_sum;
+
+public class PrefixSumArray {
+  public int[] solution(int[] array) {
+    int[] prefixSumArray = new int[array.length];
+    prefixSumArray[0] = array[0];
+    for (int i = 1; i < array.length; i++) {
+      prefixSumArray[i] = prefixSumArray[i - 1] + array[i];
+    }
+    return prefixSumArray;
+  }
 }
--- src/test/java/org/khiemtran/algorithms/prefix_sum/PrefixSumArrayTest.java
+++ src/test/java/org/khiemtran/algorithms/prefix_sum/PrefixSumArrayTest.java
@@ -1,4 +1,19 @@
-import static org.junit.jupiter.api.Assertions.*;
+package org.khiemtran.algorithms.prefix_sum;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
 class PrefixSumArrayTest {
-  
+  private final PrefixSumArray prefixSumArray;
+
+  public PrefixSumArrayTest() {
+    this.prefixSumArray = new PrefixSumArray();
+  }
+
+  @Test
+  public void case1() {
+    int[] actual = prefixSumArray.solution(new int[]{1, 2, 3, 4, 5});
+    Assertions.assertArrayEquals(new int[]{1, 3, 6, 10, 15}, actual);
+  }
 }
